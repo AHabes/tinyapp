71f77f297b5fd754cac20ae5ed4fbac76323ac0a
Abdullah Habes <abdulla096@hotmail.com>
2021-11-09 04:32:48 +0000

    add get method

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.listen(8080, () => {
+  console.log(`The server is up and listening on port ${PORT}`)
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello World!")
+});
